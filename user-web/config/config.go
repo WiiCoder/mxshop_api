3aa70fa85f4d26a15f64536d537b420e51805b9a
wii <dicoholicxxx@gmail.com>
2021-11-19 17:54:27 +0800

    feat(background):web项目工程基础框架搭建[task_0]

---
+++ user-web/config/config.go
@@ -0,0 +1,7 @@
+package config
+
+type UserSrvConfig struct {
+	Host string `mapstructure:"host" json:"host"`
+	Port int    `mapstructure:"port" json:"port"`
+	Name string `mapstructure:"name" json:"name"`
+}
