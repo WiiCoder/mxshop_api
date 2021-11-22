56c8b8dd1c7a621103801311447882153b785b54
wii <dicoholicxxx@gmail.com>
2021-11-22 17:49:55 +0800

    feat(login):添加jwt组件[task_0]

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module mxshop_api
 go 1.16
 
 require (
-	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fsnotify/fsnotify v1.5.1
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
--- user-web/config/config.go
+++ user-web/config/config.go
@@ -5,9 +5,14 @@ type UserSrvConfig struct {
 	Port int    `mapstructure:"port" json:"port"`
 }
 
+type JWTConfig struct {
+	SigningKey string `mapstructure:"key" json:"key"`
+}
+
 type ServerConfig struct {
 	Name          string        `mapstructure:"name" json:"name"`
 	Host          string        `mapstructure:"host" json:"host"`
 	Port          int           `mapstructure:"port" json:"port"`
+	JWTInfo       JWTConfig     `mapstructure:"jwt" json:"jwt"`
 	UserSrvConfig UserSrvConfig `mapstructure:"user_srv" json:"user_srv"`
 }
