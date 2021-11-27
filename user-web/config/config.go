e7928211638d412aa23a34cadc9dd7c25ad698fb
wii <dicoholicxxx@gmail.com>
2021-11-27 22:58:57 +0800

    feat(consule):consule服务发现及动态端口选择[task_0]

--- user-web/config/config.go
+++ user-web/config/config.go
@@ -3,10 +3,21 @@ package config
 type UserSrvConfig struct {
 	Host string `mapstructure:"host" json:"host"`
 	Port int    `mapstructure:"port" json:"port"`
+	Name string `mapstructure:"name" json:"name"`
 }
 
 type JWTConfig struct {
 	SigningKey string `mapstructure:"key" json:"key"`
+}
+
+type RedisConfig struct {
+	Host string `mapstructure:"host" json:"host"`
+	Port int    `mapstructure:"port" json:"port"`
+}
+
+type ConsulConfig struct {
+	Host string `mapstructure:"host" json:"host"`
+	Port int    `mapstructure:"port" json:"port"`
 }
 
 type ServerConfig struct {
@@ -15,4 +26,6 @@ type ServerConfig struct {
 	Port          int           `mapstructure:"port" json:"port"`
 	JWTInfo       JWTConfig     `mapstructure:"jwt" json:"jwt"`
 	UserSrvConfig UserSrvConfig `mapstructure:"user_srv" json:"user_srv"`
+	RedisConfig   RedisConfig   `mapstructure:"redis" json:"redis"`
+	ConsulConfig  ConsulConfig  `mapstructure:"consul" json:"consul"`
 }
--- user-web/initialize/server_conn.go
+++ user-web/initialize/server_conn.go
@@ -5,19 +5,56 @@ import (
 	"mxshop_api/user-web/global"
 	"mxshop_api/user-web/proto"
 
-	"go.uber.org/zap"
 	"google.golang.org/grpc"
+
+	"github.com/hashicorp/consul/api"
+	"go.uber.org/zap"
 )
 
 func InitSrvConn() {
-	ip := global.ServerConfig.UserSrvConfig.Host
-	port := global.ServerConfig.UserSrvConfig.Port
-	userConn, err := grpc.Dial(fmt.Sprintf("%s:%d", ip, port), grpc.WithInsecure())
+	// 1. 直连方式
+	//ip := global.ServerConfig.UserSrvConfig.Host
+	//port := global.ServerConfig.UserSrvConfig.Port
+	//userConn, err := grpc.Dial(fmt.Sprintf("%s:%d", ip, port), grpc.WithInsecure())
+	//if err != nil {
+	//	zap.S().Errorw("[GRPC Server] 连接【用户服务】失败")
+	//	return
+	//}
+	//
+	//UserSrvClient := proto.NewUserClient(userConn)
+	//global.UserSrvClient = UserSrvClient
+
+	// 2. 注册中心获取
+
+	config := api.DefaultConfig()
+	consulInfo := global.ServerConfig.ConsulConfig
+	config.Address = fmt.Sprintf("%s:%d", consulInfo.Host, consulInfo.Port)
+
+	client, err := api.NewClient(config)
+	if err != nil {
+		zap.S().Panic(err)
+	}
+
+	filterStr := fmt.Sprintf(`Service == "%s"`, global.ServerConfig.UserSrvConfig.Name)
+	data, err := client.Agent().ServicesWithFilter(filterStr)
+	if err != nil {
+		zap.S().Panic(err)
+	}
+
+	userSrvHost := ""
+	userSrvPort := 0
+
+	for _, value := range data {
+		userSrvHost = value.Address
+		userSrvPort = value.Port
+	}
+
+	userConn, err := grpc.Dial(fmt.Sprintf("%s:%d", userSrvHost, userSrvPort), grpc.WithInsecure())
 	if err != nil {
 		zap.S().Errorw("[GRPC Server] 连接【用户服务】失败")
 		return
 	}
-
 	UserSrvClient := proto.NewUserClient(userConn)
 	global.UserSrvClient = UserSrvClient
+
 }
